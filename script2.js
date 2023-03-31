ae30a07d940c4d093f41dabc067e22c6c77842c9
Mauricio Garcia <maupessoagarcia@yahoo.com>
2023-03-31 05:45:36 +0200

    Corrected hoisting problem with function expression / Added h3 change / Added input listener

--- script2.js
+++ script2.js
@@ -1,25 +1,20 @@
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
-
-let newColor1 = color1.value;
-let newColor2 = color2.value;
-
-color1.addEventListener("change", function () {
-  newColor1 = color1.value;
-  console.log(newColor1);
-  cssChange();
-});
-
-color2.addEventListener("change", function () {
-  newColor2 = color2.value;
-  console.log(newColor2);
-  cssChange();
-});
-
-// gradient.style.background = "linear-gradient(to right, black, white)";
+var css = document.querySelector("h3");
 
 const cssChange = () => {
   body.style.background =
-    "linear-gradient(to right," + newColor1 + "," + newColor2 + ")";
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+  css.textContent = body.style.background;
 };
+
+// let newColor1 = color1.value;
+// let newColor2 = color2.value;
+
+color1.addEventListener("input", cssChange);
+
+color2.addEventListener("input", cssChange);
+
+// gradient.style.background = "linear-gradient(to right, black, white)";
