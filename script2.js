03efab166e172672af8690c3d6c6cbf15a9aaefb
Mauricio Garcia <maupessoagarcia@yahoo.com>
2023-04-01 06:34:42 +0200

    Background and CSS message matches/appear from start

--- script2.js
+++ script2.js
@@ -1,15 +1,18 @@
+function cssChange() {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+  css.textContent = body.style.background;
+}
+
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
 var css = document.querySelector("h3");
 
-const cssChange = () => {
-  body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-
-  css.textContent = body.style.background;
-};
-
+body.style.background =
+  "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+css.textContent = body.style.background;
 // let newColor1 = color1.value;
 // let newColor2 = color2.value;
 
